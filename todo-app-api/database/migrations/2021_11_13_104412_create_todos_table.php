edbae5c4ed7c9113b9d844c197db22cdf957db07
RuiOkazaki <okazaki200226@gmail.com>
2021-11-17 09:05:45 +0900

    update: idをincrementで作成するのではなくidメソッドを使用することにした

--- todo-app-api/database/migrations/2021_11_13_104412_create_todos_table.php
+++ todo-app-api/database/migrations/2021_11_13_104412_create_todos_table.php
@@ -14,7 +14,7 @@ class CreateTodosTable extends Migration
     public function up()
     {
         Schema::create('todos', function (Blueprint $table) {
-            $table->increments("id");
+            $table->id();
             $table->string("title");
             $table->text("text")->nullable();
             $table->timestamps();
