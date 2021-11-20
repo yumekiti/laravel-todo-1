0873859406e17f4dc54533cd011f938cd4b8dcf5
RuiOkazaki <okazaki200226@gmail.com>
2021-11-20 22:09:49 +0900

    add: todoの seeder を作成した

---
+++ todo-app-api/database/seeders/TodoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Todo;
+
+class TodoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Todo::factory()->create([
+            "title" => "sample title",
+            "text" => "sample text",
+            "user_id" => 1
+        ]);
+    }
+}
