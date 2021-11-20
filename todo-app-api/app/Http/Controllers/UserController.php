a085aa7bf5f3745367465d9fbce05ae5fcc9a70c
RuiOkazaki <okazaki200226@gmail.com>
2021-11-20 22:07:06 +0900

    add: UserControllerを作成した

---
+++ todo-app-api/app/Http/Controllers/UserController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // 現在ログインしているユーザーを取得する
+        return Auth::user();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        return User::created([
+            "name" => $request->input("name"),
+            "email" => $request->input("email"),
+            "password" => $request->input("password")
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        return Auth::user()->update([
+            "name" => $request->input("name"),
+            "email" => $request->input("email"),
+            "password" => $request->input("password")
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+        return Auth::user()->delete();
+    }
+
+    public function login(Request $request)
+    {
+        if(Auth::attempt($request->all()))
+        {
+            return Auth::user();
+        }
+    }
+}
