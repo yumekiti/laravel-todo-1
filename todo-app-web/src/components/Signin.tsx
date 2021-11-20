4f871a590bfedebc5916702f93bf2f053f6ab5ad
yumekiti <yumekiti120440@gmail.com>
2021-11-21 02:05:15 +0900

    add: web-login

--- todo-app-web/src/components/Signin.tsx
+++ todo-app-web/src/components/Signin.tsx
@@ -1,10 +1,27 @@
-import { VFC } from 'react';
+import { useState, useEffect, VFC } from 'react';
+import axios from 'axios';
 
 const Signin: VFC = () => {
+
+  const [datas, setDatas] = useState([])
+
+  const login = () => {
+    const formData = {
+        email: 'test1@test.jp',
+        password: 'testtest',
+    }
+    axios.get('http://127.0.0.1:8000/api/csrf-cookie').then(() => {
+      axios
+        .post('http://127.0.0.1:8000/api/login', formData)
+        .then(res => (setDatas(res.data)))
+    });
+  }
   return (
     <>
       <h2>signin</h2>
       <p>signin des</p>
+      <p>{datas}</p>
+      <button onClick={() => login()}>login</button>
     </>
   );
 };
