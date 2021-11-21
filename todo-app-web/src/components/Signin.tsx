53af53652a4b479705924df6e2ee8dec1bb922f3
yumekiti <yumekiti120440@gmail.com>
2021-11-21 19:04:16 +0900

    add: getuser

--- todo-app-web/src/components/Signin.tsx
+++ todo-app-web/src/components/Signin.tsx
@@ -1,4 +1,4 @@
-import { useState, VFC } from 'react';
+import { VFC } from 'react';
 import axios from 'axios';
 
 const Signin: VFC = () => {
@@ -8,17 +8,25 @@ const Signin: VFC = () => {
       email: 'test1@test.jp',
       password: 'testtest',
     }
-    axios.get('localhost:8080/api/csrf-cookie').then(() => {
+    axios.get('/api/csrf-cookie').then(() => {
       axios
-        .post('localhost:8080/api/users/login', formData)
+        .post('/api/users/login', formData)
         .then(res => (console.log(res.data)))
     });
   }
+
+  const getUser = () => {
+    axios
+      .get('/api/users')
+      .then(res => (console.log(res.data)))
+  }
+
   return (
     <>
       <h2>signin</h2>
       <p>signin des</p>
       <button onClick={() => login()}>login</button>
+      <button onClick={() => getUser()}>get user</button>
     </>
   );
 };
